071f5a1e58c358d477de69ea42fd8fd814e3530f
micaelllevi <68759448+micaelllevi@users.noreply.github.com>
2025-09-22 18:33:51 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("contador-v1").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icons/icon-192.png",
+        "icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
